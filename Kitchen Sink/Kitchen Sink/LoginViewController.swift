94a1f5cb2243ef3d54d7463fc8d1e1ed1938faf7
Jibraan Siddiqi <jibraan@utexas.edu>
2021-11-11 13:27:37 -0600

    added Firebase and pods and Kitchen Sink.xcworkspace

--- Kitchen Sink/Kitchen Sink/LoginViewController.swift
+++ Kitchen Sink/Kitchen Sink/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
---
+++ Kitchen Sink/Pods/Target Support Files/Pods-Kitchen Sink/Pods-Kitchen Sink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kitchen_Sink : NSObject
+@end
+@implementation PodsDummy_Pods_Kitchen_Sink
+@end
