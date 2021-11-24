a7090709606e13e27c8062e5b7d03319f4aa3ff7
Roger Perales II <rogerperalesii@Rogers-MacBook-Pro.local>
2021-11-24 15:45:04 -0600

    recipe class, some buttons

---
+++ Kitchen Sink/Kitchen Sink/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  Kitchen Sink
+//
+//  Created by Samuel Perales on 11/24/21.
+//
+
+import UIKit
+
+class Recipe {
+    var name = ""
+    var description = ""
+    var type = "breakfast" //breakfast, lunch, dinner
+    var ingredients = [""]
+    var directions = [""]
+}
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
