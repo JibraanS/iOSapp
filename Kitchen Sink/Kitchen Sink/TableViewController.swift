0eacc5b7ab4f11045f7714cf4752371c682bd592
Roger Perales II <rogerperalesii@wireless-10-147-151-10.public.utexas.edu>
2021-11-28 21:02:59 -0600

    darkmode for table view and recipe page

--- Kitchen Sink/Kitchen Sink/TableViewController.swift
+++ Kitchen Sink/Kitchen Sink/TableViewController.swift
@@ -28,9 +28,11 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         // set background based on darkmode
         if (UserDefaults.standard.bool(forKey: email + "darkmode")) {
             view.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.1960784314, blue: 0.1960784314, alpha: 1)
+            tableView.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.1960784314, blue: 0.1960784314, alpha: 1)
         }
         else {
             view.backgroundColor = #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
+            tableView.backgroundColor = #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
         }
     }
     
@@ -61,6 +63,20 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         let row = indexPath.row
         cell.textLabel!.numberOfLines = 5
         cell.textLabel?.text = displayed_recipes[row].name
+        //cell.backgroundColor = UIColor.clear
+        
+        // get current user
+        let user = Auth.auth().currentUser
+        let email:String = user?.email ?? "none"
+        
+        if (UserDefaults.standard.bool(forKey: email + "darkmode")) {
+            cell.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.1960784314, blue: 0.1960784314, alpha: 1)
+            cell.textLabel?.textColor = #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
+        }
+        else {
+            cell.backgroundColor = #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
+            cell.textLabel?.textColor = #colorLiteral(red: 0.1960784314, green: 0.1960784314, blue: 0.1960784314, alpha: 1)
+        }
         return cell
     }
 }
